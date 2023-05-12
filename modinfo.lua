a177dff7e70f81c5254b45445aaec3c40556966d
罗川晶(28902) <704863851@qq.com>
2023-05-12 14:37:07 +0800

    初始化项目

---
+++ modinfo.lua
@@ -0,0 +1,26 @@
+name = "health zero"
+description = "食物不回血"
+author = "天才派大星"
+version = "0.0.1"
+
+forumthread = ""
+
+api_version = 6
+dst_api_version = 10 -- line added for forum thread; is not like this in mod currently
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+dst_compatible = true
+
+all_clients_require_mod = false
+client_only_mod = false
+
+server_filter_tags = {}
+
+priority = -996 --优先级，值越大越先加载 默认0
+
+icon_atlas = "_patrick.xml"
+icon = "_patrick.tex"
+
+configuration_options = {}
---
+++ modmain.lua
@@ -0,0 +1,5 @@
+AddComponentPostInit("edible", function(self)
+     function self:GetHealth(eater)
+        return 0
+    end
+end)
